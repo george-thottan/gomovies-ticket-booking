2701c39270cc43e311b3eeaedb8e37cb560e17ff
George_Martin <grgmrtn2312@gmail.com>
2017-11-01 15:58:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var app = express();
+var handlebars = require('express3-handlebars').create({ defaultLayout:'themain' });
+var mysql= require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'docterjose',
+  database: 'gomovies'
+})
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log('You are now connected...');
+    });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', process.env.PORT || 8080);
+
+app.use(express.static(__dirname + '/public'));
+const fileUpload = require('express-fileupload');
+app.use(fileUpload());
+app.use(require('body-parser')());
+
+app.get('/', function(req,res){
+	res.render('sen',{ title: 'hi there'});
+})
+
+app.post('/upload', function(req, res) {
+	var mname= req.body.mname;
+	var message= req.body.message;
+	var stat= req.body.stat;
+	var num;
+	if (!req.files.sampleFile)
+    return res.status(400).send('No files were uploaded.');
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+	let sampleFile = req.files.sampleFile;
+
+  // Use the mv() method to place the file somewhere on your server
+	sampleFile.mv(__dirname+'/public/img/uploads/'+sampleFile.name, function(err) {
+    if (err)
+      return res.status(500).send(err);
+	if(stat== 'running')
+		num=1;
+	else
+		num=0;
+	var minfo= [[req.body.mname,sampleFile.name,req.body.message,num]];
+	connection.query("INSERT INTO movies(name,img,abt,status) VALUES ?",[minfo], function(err, results, fields){
+		if (err)
+			throw err;
+		else
+			res.send('File uploaded!');
+	});
+
+  });
+});
+
+// custom 404 page
+app.use(function(req, res){
+	res.type('text/plain');
+	res.status(404);
+	res.send('404 - Not Found');
+});
+
+// custom 500 page
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.type('text/plain');
+	res.status(500);
+	res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function(){
+	console.log( 'Express started on http://localhost:' +
+	app.get('port') + '; press Ctrl-C to terminate.' );
+});
---
+++ server.js
@@ -0,0 +1,141 @@
+var express = require('express');
+var app = express();
+var crypto = require('crypto');
+var mysql= require('mysql');
+var session= require('express-session');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'docterjose',
+  database: 'gomovies'
+})
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log('You are now connected...');
+    });
+// set up handlebars view engine
+var handlebars = require('express3-handlebars').create({ defaultLayout:'scndry' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', process.env.PORT || 3000);
+app.use(require('body-parser')());
+app.use(express.static(__dirname + '/public'));
+app.use(session({
+	secret : 'someRandomSecretValueMateyy',
+	cookie:{maxAge:1000*60*60*24*30}
+}));
+app.get('/', function(req, res){
+	res.render('GoMovies',{layout : "main"});
+});
+app.get('/movies',function(req, res){
+	if(req.session && req.session.auth && req.session.auth.userId){
+		var runnin=1;
+		
+		connection.query("SELECT * from movies where status=?",[runnin],function(err,results){
+		if (err) throw err;
+		if(results.length>0){
+			
+			res.render('movie',{results: results});}
+		else{
+			console.log("Not found");
+			res.status(404);
+			res.send('srry');
+		  }
+	  });
+	}//res.render('movie',{layout : "scndry"});
+	else{
+		res.send("Please do login");
+	}
+});
+app.get('/register',function(req, res){
+	res.render('register',{layout : "main"});
+});
+
+
+app.get('/page',function(req, res){
+	res.render('moviepage');
+});
+
+
+function hash(input,salt){
+	var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512,'sha512');
+	return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
+}
+
+app.post('/create-user',function(req,res){
+		var fname=req.body.fname;
+		var lname=req.body.lname;
+		var reg_email=req.body.reg_email;
+		var pass=req.body.reg_pass;
+		var conf_pass=req.body.reg_conf;
+		var phone=req.body.phone;
+		var salt= crypto.randomBytes(128).toString('hex');
+		var dbString=hash(pass,salt);
+		var meminfo=[[fname,lname,reg_email,dbString,phone]];
+		connection.query("INSERT INTO members(fname,lname,elmail,passwrd,phone) VALUES ?",[meminfo], function(err, results){
+		if (err)
+			throw err;
+		else
+			res.send('success');
+	});
+    
+});
+
+app.post('/signin',function(req,res){
+	var sign_email= req.body.s_email;
+	var sign_pass= req.body.s_pass;
+	
+	connection.query("SELECT * FROM members WHERE elmail=?",[sign_email],function(err, results){
+		if (err)
+			throw err;
+		else
+		{
+
+			if(results.length === 0)
+			{
+				res.send('username/password is invalid');
+			}
+			else
+			{
+				var dbString= results[0].passwrd;
+				var salt = dbString.split("$")[2];
+				var hashpass= hash(sign_pass, salt);
+				if(hashpass===dbString)
+				{
+					req.session.auth={userId : results[0].mem_id}
+					res.redirect(303,'/movies');
+				}
+				else
+				{
+					res.send('username/password is invalid');
+				}
+			}
+		}
+	})
+});
+
+app.get('/logout', function(req,res){
+	delete req.session.auth;
+	res.redirect(303,'/');
+});
+
+// custom 404 page
+app.use(function(req, res){
+	res.type('text/plain');
+	res.status(404);
+	res.send('404 - Not Found');
+});
+
+// custom 500 page
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.type('text/plain');
+	res.status(500);
+	res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function(){
+	console.log( 'Express started on http://localhost:' +
+	app.get('port') + '; press Ctrl-C to terminate.' );
+});
